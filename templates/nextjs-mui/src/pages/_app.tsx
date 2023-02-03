10f183d1a4f76ed7e22e595275ec26237014db5b
Eunchurn Park <eunchurn.park@gmail.com>
2023-02-03 12:15:26 +0900

    feat(theme): update from builderhub/mui-theme

--- templates/nextjs-mui/src/pages/_app.tsx
+++ templates/nextjs-mui/src/pages/_app.tsx
@@ -2,10 +2,9 @@ import * as React from "react";
 import "../styles/globals.css";
 import Head from "next/head";
 import { AppProps } from "next/app";
-import { ThemeProvider } from "@mui/material/styles";
+import { BuilderhubThemeProvider } from "@builderhub/mui-theme";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import theme from "../theme";
 import createEmotionCache from "../createEmotionCache";
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -22,11 +21,11 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
+      <BuilderhubThemeProvider>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
-      </ThemeProvider>
+      </BuilderhubThemeProvider>
     </CacheProvider>
   );
 }
